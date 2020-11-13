e476e42ac98e880bbf8859a23a9c180068a69b0f
Esteban Salas <esalas21@uan.edu.co>
2020-11-12 20:32:20 -0500

    Se guardan la calificación con las preferencias

--- app/src/main/java/com/example/friends/User.kt
+++ app/src/main/java/com/example/friends/User.kt
@@ -1,11 +1,17 @@
 package com.example.friends
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.preference.PreferenceManager
+import android.view.KeyEvent
 import kotlinx.android.synthetic.main.activity_user.*
 
 class User : AppCompatActivity() {
+
+    private val key = "MY_KEY"
+    private var score = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
@@ -14,11 +20,27 @@ class User : AppCompatActivity() {
         val image = imageIn?.toInt()
         val description = intent.getStringExtra("description")
 
-        Log.v("parameterId", "$imageIn")
-        Log.v("parametersDes", "$description")
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val value = preferences.getFloat(key, score)
 
         imageView.setImageResource(image!!)
-
         textDescription.text = description.toString()
+        calification.rating = value
+
+        calification.setOnRatingBarChangeListener { ratingBar, fl, b ->
+            score = fl
+            val editor = preferences.edit()
+            editor.putFloat(key, fl)
+            editor.apply()
+        }
     }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        return super.onKeyDown(keyCode, event)
+
+        val mainActivity = Intent(this, MainActivity::class.java)
+        mainActivity.putExtra("score", score)
+        startActivity(mainActivity)
+    }
+
 }
